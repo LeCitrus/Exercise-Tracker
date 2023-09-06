66f2b650ab38511978e7879da14168e498efaf38
Curtis Kan <curtis.tl.kan@gmail.com>
2023-09-06 00:17:09 -0600

    basic functionalities

--- lib/exercise_item.dart
+++ lib/exercise_item.dart
@@ -1,11 +1,114 @@
 import 'package:flutter/material.dart';
 
-class ExerciseItem extends StatelessWidget {
-  const ExerciseItem({super.key});
+class ExerciseItem extends StatefulWidget{
+
+  const ExerciseItem({super.key,
+    this.id = '',
+    this.name = '',
+    this.amount = 0,
+    required this.removeItem});
+
+  final String name, id;
+  final int amount;
+  final RemoveCallback removeItem;
+
+  @override
+  State<ExerciseItem> createState() => _ExerciseItemState();
+}
+
+class _ExerciseItemState extends State<ExerciseItem> {
+
+  String name = '';
+  int amount = 0;
+  String nameVal = '';
+  int amountVal = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    name = widget.name;
+    amount = widget.amount;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello");
-  }
+      return GestureDetector(
+          onTap: () {showDialog(context: context, builder: (context) {
+            return AlertDialog(
+                title: Text("Add Item", style: TextStyle(color: Colors.white)),
+                backgroundColor: Colors.blueGrey,
+                content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget> [
+                      TextField(
+                        controller: TextEditingController()..text = name,
+                        onChanged: (value) {
+                          setState(() {
+                            nameVal = value;
+                          });
+                        },
+                      ),
+                      TextField(
+                        controller: TextEditingController()..text = '$amount',
+                        keyboardType: TextInputType.number,
+                        onChanged: (value) {
+                          setState(() {
+                            amountVal = int.parse(value);
+                          });
+                        },
+                      ),]),
+                actions: [
+                  TextButton(
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white54)),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }, child: Text("Cancel"),)
+                  ,
+                  TextButton(
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white54)),
+                    onPressed: () {setState(() {
+                      name = nameVal;
+                      amount = amountVal;
+                      Navigator.pop(context);
+                      });
+                    },
+                    child: Text("Confirm"),)
+                  ]
+                );
+              }
+            );
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.red,
+            ),
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(10),
+            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget> [
+              Row(mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget> [Text('${amount} ', style: TextStyle(
+                  fontSize: 30,
+                  wordSpacing: 20,
+                  fontFamily: 'Barlow'
+                ),
+              ),
+              Text(name, style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'Barlow',
+              ))]),
+              FloatingActionButton(
+                  onPressed: (() {
+                    widget.removeItem(widget.id);
+                  }),
 
+                  child: Icon(Icons.delete_outline),)
+            ]
+            ),
+        )
+      );
+    }
 }
+
+typedef RemoveCallback = void Function(String id);
--- lib/main.dart
+++ lib/main.dart
@@ -5,36 +5,94 @@ void main() => runApp(MaterialApp(
   home: Home(),
 ));
 
-
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List<ExerciseItem> items = [];
+  String nameValue = '';
+  int amountValue = 0;
+
+  void addItem(String name, int amount) {
+    setState(() {items.add(ExerciseItem(id: '${items.length}', name: name, amount: amount, removeItem: (String id) {
+      setState(() {
+        items.removeWhere((item) => item.id == id);
+          });
+        })
+      )
+      ;})
+    ;}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Exercise Tracker',
-        style: TextStyle(fontSize: 25, color: Colors.white,)),
-        centerTitle: true,
-        backgroundColor: Colors.black26,
+      title: Text('Exercise Tracker',
+      style: TextStyle(fontSize: 25, color: Colors.white, fontFamily: 'Barlow')),
+      centerTitle: true,
+      backgroundColor: Colors.black26,
 
       ),
-      body: Row(children: <Widget> [Center(
-        child: Text('hello ninjas!',
-          style: TextStyle(
-            fontSize: 50,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 5,
-            color: Colors.green,
-            fontFamily: 'Barlow',
-          ),
-        ),
-      ), ExerciseItem(),]
-      ),
+      body: ListView(children: <Widget>[...items]),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('addmer'),
-        backgroundColor: Colors.black26,
+      onPressed: (() {showDialog(context: context, builder: (context) {
+        return AlertDialog(
+          title: Text("Add Item", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blueGrey,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget> [
+            TextField(
+            onChanged: (value) {
+              setState(() {
+                nameValue = value;
+              });
+            },
+            decoration: InputDecoration(
+              hintText: "Enter exercise name",
+              hintStyle: TextStyle(color: Colors.white)
+            ),
+          ),
+            TextField(
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  amountValue = int.parse(value);
+                });
+              },
+              decoration: InputDecoration(
+                hintText: "Enter amount done",
+                  hintStyle: TextStyle(color: Colors.white),
+              ),
+            ),]),
+          actions: [
+            TextButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white54)),
+              onPressed: () {
+              Navigator.pop(context);
+            }, child: Text("Cancel"),)
+            ,
+            TextButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white54)),
+              onPressed: () {
+              setState(() {
+                addItem(nameValue, amountValue);
+                Navigator.pop(context);
+              });
+            }, child: Text("Confirm"),)
+          ]
+        );
+
+          }
+        );
+      }),
+      backgroundColor: Colors.green,
+      child: Icon(Icons.add),
       ),
     );
   }
